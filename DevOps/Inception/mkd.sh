e72d1f8d1d81e672f7aa3ee7d77495dd7f38d8e3
Carlo Bilbo <cbilbo@mi-a5.msk.21-school.ru>
2022-02-17 19:26:36 +0300

    check minishell

---
+++ DevOps/Inception/mkd.sh
@@ -0,0 +1,15 @@
+if ! [ -d $HOME/data/mysite ]; then
+	mkdir -p $HOME/data/mysite
+fi
+if ! [ -d $HOME/data/mariadb ]; then
+	mkdir -p $HOME/data/mariadb
+fi
+if ! [ -d $HOME/data/wordpress ]; then
+	mkdir -p $HOME/data/wordpress
+fi
+if ! [ -d $HOME/data/adminer ]; then
+	mkdir -p $HOME/data/adminer
+fi
+if ! [ -d $HOME/data/redis ]; then
+	mkdir -p $HOME/data/redis
+fi
